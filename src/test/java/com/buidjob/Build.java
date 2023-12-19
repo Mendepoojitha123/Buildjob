9e15a7314fe0ee32a59156a8aad7c259f1f46d9e
Mendepoojitha123 <153059453+Mendepoojitha123@users.noreply.github.com>
2023-12-19 12:15:51 +0530

    Add files via upload
    adding my buildjob

---
+++ src/test/java/com/buidjob/Build.java
@@ -0,0 +1,11 @@
+package com.buidjob;
+
+public class Build {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" hi i am developer this is my build job");
+
+	}
+
+}
